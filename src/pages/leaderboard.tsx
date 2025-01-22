c97b82c09f2cbae62bac49ee51df4f2c5e5b2a62
Bakpo Godwin <godwinfriday31@gmail.com>
2025-01-22 15:18:35 +0100

    leaderboard headerand footer text added

--- src/pages/leaderboard.tsx
+++ src/pages/leaderboard.tsx
@@ -119,8 +119,14 @@ export const Leaderboard: React.FC = () => {
   }
 
   return (
-    <div className="mx-auto py-10 px-4 max-w-3xl">
-      {data && <DataTable columns={columns} data={data} />}
+    <div>
+      <h1 className="max-w-[300px] md:max-w-[746px] mx-auto text-2xl md:text-5xl text-center champ-black text-white font-bold">
+        Leaderboard
+      </h1>
+      <div className="mx-auto py-10 px-4 max-w-3xl">
+        {data && <DataTable columns={columns} data={data} />}
+      </div>
+      <p>Built with ❤️ by Superteam Ekiti</p>
     </div>
   );
 };
