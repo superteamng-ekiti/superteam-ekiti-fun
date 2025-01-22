2dcf6168634d893e5aab81ee7d213ba70bdca31e
Bakpo Godwin <godwinfriday31@gmail.com>
2025-01-22 16:25:11 +0100

    another commit

--- src/pages/leaderboard.tsx
+++ src/pages/leaderboard.tsx
@@ -123,10 +123,12 @@ export const Leaderboard: React.FC = () => {
       <h1 className="max-w-[300px] md:max-w-[746px] mx-auto text-2xl md:text-5xl text-center champ-black text-white font-bold">
         Leaderboard
       </h1>
-      <div className="mx-auto py-10 px-4 max-w-3xl">
+      <div className="mx-auto py-8 px-4 max-w-3xl">
         {data && <DataTable columns={columns} data={data} />}
       </div>
-      <p>Built with ❤️ by Superteam Ekiti</p>
+      <p className="text-center flex justify-center">
+        Built with ❤️ by Superteam Ekiti
+      </p>
     </div>
   );
 };
