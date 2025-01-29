fd5c21643f50bd47f46ff7a1ebc94f948fc32685
EmmanuelOluwafemi <emaz4me@gmail.com>
2025-01-29 10:32:46 +0100

    feat: implemented share button

--- src/components/user-details/user-points.tsx
+++ src/components/user-details/user-points.tsx
@@ -1,27 +1,33 @@
 import { Loader2 } from "lucide-react";
-import { Button } from "../ui/button"
+import { Button } from "../ui/button";
 import { useUser } from "@/context/user.context";
-export const UserPoints = ({ isLoading, handleCheckRepository }: { isLoading: boolean, handleCheckRepository: () => void }) => {
+import { ShareButton } from "./share-button";
+export const UserPoints = ({
+  isLoading,
+  handleCheckRepository,
+}: {
+  isLoading: boolean;
+  handleCheckRepository: () => void;
+}) => {
   const { user } = useUser();
   return (
     <div className="flex flex-col items-center justify-center mt-12">
-        <p className="text-white text-xl work-sans-regular">
-            User Points
-        </p>
+      <p className="text-white text-xl work-sans-regular">User Points</p>
 
-        <h3 className="text-white text-7xl champ-black text-center">{user?.points}</h3>
+      <h3 className="text-white text-7xl champ-black text-center">
+        {user?.points}
+      </h3>
 
-        <div className="flex items-center gap-4 mt-6">
-            <Button size="sm" disabled={isLoading} onClick={handleCheckRepository}>
-              Update Point
-              {isLoading && <Loader2 className="w-4 h-4 animate-spin" />}
-            </Button>
-            <Button size="sm" variant="secondary">
-                Share on
-
-                <img src="/src/assets/images/twitter.svg" alt="twitter" className="w-5 h-5" />
-            </Button>
-        </div>
+      <div className="flex items-center gap-4 mt-6">
+        <Button size="sm" disabled={isLoading} onClick={handleCheckRepository}>
+          Update Point
+          {isLoading && <Loader2 className="w-4 h-4 animate-spin" />}
+        </Button>
+        <ShareButton
+          title={`I'm building on Solana and I've earned ${user?.points} points with Reward Dev!`}
+          url={`https://reward-dev.fun?ref=${user?.referral_code}`}
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
