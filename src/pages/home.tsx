524a25fd15bdca3a17f315f14daedec4e2f0ffc8
Elijah <edunelijah18@gmail.com>
2025-01-29 10:57:31 +0100

    fix: merge conflicts

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,11 +1,9 @@
 import { Hero } from "@/components/home/hero";
-import Repositories from "@/components/Repositories";
 
 export default function Home() {
   return (
     <div className="w-full h-full relative">
       <Hero />
-      <Repositories />
     </div>
   );
 }
